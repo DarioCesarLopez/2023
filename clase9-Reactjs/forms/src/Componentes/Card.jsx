898877411dc4b4009f016c77fc367be3d79d1eb2
Dario-Lopez-TDF <dariolopez@tdf.edu.ar>
2023-04-16 16:04:41 -0300

    Formulario completo en React

--- clase9-Reactjs/forms/src/Componentes/Card.jsx
+++ clase9-Reactjs/forms/src/Componentes/Card.jsx
@@ -1,14 +1,18 @@
+import './Card.css';
+
 export const Card = (props) => {
 
     return (
         <>
-            <div>
-                <h1>{props.nombre}</h1>
-                <h3>{props.apellido}</h3>
-            
+            <div className='tarjeta'>
+                <h3>Nombre: {props.nombre}</h3>
+                <h3>Apellido: {props.apellido}</h3>
+                <h3>Nacionalidad: {props.nacionalidad}</h3>
+                <h3>DNI: {props.dni}</h3>
+                <h3>Edad: {props.edad}</h3>
+                <p>Ocupación: {props.descripcion}</p>
             </div>        
-        
-        
+               
         </>
     )
 
