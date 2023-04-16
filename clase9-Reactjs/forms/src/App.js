be685a9e8e4e727ee187107fe2c65d72a57f4906
Dario-Lopez-TDF <dariolopez@tdf.edu.ar>
2023-04-16 11:30:57 -0300

    Forms en React

---
+++ clase9-Reactjs/forms/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MyForm from './Componentes/formulario';
+
+function App() {
+  return (
+    <div className="App">
+     <MyForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ clase9-Reactjs/forms/src/Componentes/Card.jsx
@@ -0,0 +1,15 @@
+export const Card = (props) => {
+
+    return (
+        <>
+            <div>
+                <h1>{props.nombre}</h1>
+                <h3>{props.apellido}</h3>
+            
+            </div>        
+        
+        
+        </>
+    )
+
+}
